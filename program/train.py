badf193c0907d633395dfbe38564bf1646053488
donyori <donyoridoyodoyo@outlook.com>
2018-11-06 21:18:20 +0800

    Modify train.py for time_limit.

--- program/train.py
+++ program/train.py
@@ -9,12 +9,14 @@ def _main():
                         help='The name of the model to be trained.', default=None, required=False)
     parser.add_argument('-e', '--end_epoch', type=int, dest='end_epoch',
                         help='The end epoch of train.', default=1, required=False)
+    parser.add_argument('-t', '--time_limit', type=str, dest='time_limit',
+                        help='Time limit of train.', default=None, required=False)
     args = parser.parse_args()
     print('Start training.')
     if args.model is not None:
-        resume_training_model(model_name=args.model, end_epoch=args.end_epoch)
+        resume_training_model(model_name=args.model, end_epoch=args.end_epoch, time_limit=args.time_limit)
     else:
-        resume_training_latest_model(end_epoch=args.end_epoch)
+        resume_training_latest_model(end_epoch=args.end_epoch, time_limit=args.time_limit)
     print('Done.')
 
 
